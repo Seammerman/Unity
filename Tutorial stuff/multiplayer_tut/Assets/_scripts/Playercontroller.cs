33f2efa6a7ce0b12cb31b8270496d1a5dff21166
Spencer <seammerman@gmail.com>
2017-07-13 00:35:16 -0400

    panels

--- Tutorial stuff/multiplayer_tut/Assets/_scripts/Playercontroller.cs
+++ Tutorial stuff/multiplayer_tut/Assets/_scripts/Playercontroller.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
 using UnityEngine;
 
-public class Playercontroller : MonoBehaviour
+public class Playercontroller : NetworkBehaviour
 {
     public float walkspeed = 3.0f;
     public float turnspeed = 150.0f;
  
-
     void Update()
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnspeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * walkspeed;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
-
-        
+    
     }
+
 
 }
