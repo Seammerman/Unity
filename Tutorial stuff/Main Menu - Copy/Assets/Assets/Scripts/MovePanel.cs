a37ee14f75e9481ab513e50f8e00fc2820b8b767
Seammerman <seammerman@gmail.com>
2017-07-15 02:17:01 -0400

    panelmovement

---
+++ Tutorial stuff/Main Menu - Copy/Assets/Assets/Scripts/MovePanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePanel : MonoBehaviour {
+    public float fps = 30.0f;
+    public float movetime = 1.0f;
+
+    public GameObject RefPanel;
+    public GameObject TargetPanel;
+
+    private Vector3 velocity;
+    private Vector3 startpos;
+    private Vector3 endpos;
+    private Vector3 delpos;
+    private bool clicked = false;
+    // Use this for initialization
+    private void Start()
+    {
+        startpos = TargetPanel.transform.position;
+        endpos = RefPanel.transform.position;
+        velocity = (endpos - startpos) / (fps * movetime);
+
+    }
+    public void click () {
+        clicked = true;
+    if (TargetPanel.transform.position != startpos)
+            {
+                TargetPanel.transform.position = startpos;
+            }
+    else if(TargetPanel.transform.position != endpos)
+        {
+            TargetPanel.transform.position = endpos;
+        }
+    }
+    private void Update()
+    {
+        if (GetComponent(Button) == false)
+        {
+            TargetPanel.transform.position = startpos;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
